7829657d6a31d4fa97907c5388d4f945c465863a
Bhabani Sankar Prusty <bhabani@olacabs.com>
2017-03-23 20:06:22 +0530

    added swift 3.0 #keyPath()

--- Shopping Cart/Shopping Cart/ViewController/SCProductDetailViewController.swift
+++ Shopping Cart/Shopping Cart/ViewController/SCProductDetailViewController.swift
@@ -10,12 +10,6 @@ import UIKit
 
 final class SCProductDetailViewController: SCDetailViewController {
     
-    private struct Static {
-        static let cartQuantityKeyPathIdentifier = "cart.quantity"
-        static let availableQuantityKeyPathIdentifier = "storage.availableQuantity"
-    }
-    
-    
     @IBOutlet weak var addtoCartButton: UIButton!
     @IBOutlet weak var detailDescriptionLabel: UILabel!
     @IBOutlet weak var catalogImageView: UIImageView!
@@ -29,12 +23,12 @@ final class SCProductDetailViewController: SCDetailViewController {
     
     override var catalog: ProductCatalog? {
         willSet{
-            catalog?.removeObserver(self, forKeyPath: Static.cartQuantityKeyPathIdentifier)
-            catalog?.removeObserver(self, forKeyPath: Static.availableQuantityKeyPathIdentifier)
-            newValue?.addObserver(self, forKeyPath: Static.cartQuantityKeyPathIdentifier,
+            catalog?.removeObserver(self, forKeyPath: #keyPath(ProductCatalog.cart.quantity))
+            catalog?.removeObserver(self, forKeyPath: #keyPath(ProductCatalog.storage.availableQuantity))
+            newValue?.addObserver(self, forKeyPath: #keyPath(ProductCatalog.cart.quantity),
                                   options: .new,
                                   context: nil)
-            newValue?.addObserver(self, forKeyPath: Static.availableQuantityKeyPathIdentifier,
+            newValue?.addObserver(self, forKeyPath: #keyPath(ProductCatalog.storage.availableQuantity),
                                   options: .new,
                                   context: nil)
         }
@@ -120,15 +114,15 @@ final class SCProductDetailViewController: SCDetailViewController {
     }
     
     override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
-        if keyPath == Static.cartQuantityKeyPathIdentifier{
+        if keyPath == #keyPath(ProductCatalog.cart.quantity){
             animateValueChange(label: itemsCoutInCartLabel, value: String(change?[NSKeyValueChangeKey.newKey] as? Double ?? 0))
-        }else if keyPath == Static.availableQuantityKeyPathIdentifier{
+        }else if keyPath == #keyPath(ProductCatalog.storage.availableQuantity){
             animateValueChange(label: itemsStockLabel, value: String(change?[NSKeyValueChangeKey.newKey] as? Double ?? 0))
         }
     }
     
     deinit {
-        catalog?.removeObserver(self, forKeyPath: Static.cartQuantityKeyPathIdentifier)
-        catalog?.removeObserver(self, forKeyPath: Static.availableQuantityKeyPathIdentifier)
+        catalog?.removeObserver(self, forKeyPath: #keyPath(ProductCatalog.cart.quantity))
+        catalog?.removeObserver(self, forKeyPath: #keyPath(ProductCatalog.storage.availableQuantity))
     }
 }
